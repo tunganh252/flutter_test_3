46c6adc1b5e3bc04a8058ddd073745b46093d872
tunganh252 <tunganh2521999@gmail.com>
2020-11-27 23:31:28 +0700

    [Update] - listView

---
+++ lib/categories_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import './category_item.dart';
+import './dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gkitchen'),
+      ),
+      body: GridView(
+          children: DUMMY_CATEGORIES
+              .map((e) => CategoryItem(e.color, e.title))
+              .toList(),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20)),
+    );
+  }
+}
---
+++ lib/dummy_data.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(id: 'a1', title: "Name 111", color: Colors.green),
+  Category(id: 'a2', title: "Name 222", color: Colors.blue),
+  Category(id: 'a3', title: "Name 333", color: Colors.red),
+  Category(id: 'a4', title: "Name 444", color: Colors.yellow),
+  Category(id: 'a5', title: "Name 555", color: Colors.pink),
+  Category(id: 'a6', title: "Name 666", color: Colors.brown),
+];
